9a6dbc93d78d1a5eece4cfc6862aac95721d27dc
Amalia Litsa <1714197+alitsa@users.noreply.github.com>
2019-05-15 18:11:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Unicorn email templates</title>
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<base target="_blank">
+<style type="text/css">
+  html, body {font-family: 'open sans', sans-serif; color: #002838; line-height: 1.6em; margin: 2em;}
+  a {color: #7E5CEF; text-decoration: none;}
+</style>
+</head>
+
+<body>
+  <h1>Unicorn email templates</h1>
+  <p>
+    These templates standardize the way our products communicate with their users.
+  </p>
+  <h2>Templates</h2>
+  <p>
+    Coming soon
+  </p>
+  <h2>Examples</h2>
+<ul>
+  <li><a href="example_transactional-email.html">Example transactional email</a></li>
+  <li><a href="example_insights-email.html">Example insights email</a></li>
+</ul>
+</body>
+
+</html>
