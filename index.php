b630ca420e89fa78756c94d573a56e4abeb36a92
Amalia Litsa <amalia.litsa@wpengine.com>
2019-05-15 13:56:04 -0500

    replaced illustration with logo

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-  echo "hi";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Unicorn email templates</title>
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<base target="_blank">
+<style type="text/css">
+  html, body {font-family: 'open sans', sans-serif; color: #002838; line-height: 1.6em; margin: 2em;}
+  a {color: #7E5CEF; text-decoration: none;}
+</style>
+</head>
+
+<body>
+  <h1>Unicorn email templates</h1>
+  <p>
+    Receiving and responding to email is frequently an important step in tasks our customers want to accomplish. We've provided these templates as part of an overall effort to standardize the way our product communicates with customers.
+  </p>
+  <h2>Templates</h2>
+  <p>
+    Coming soon
+  </p>
+  <h2>Examples</h2>
+<ul>
+  <li><a href="example_transactional-email.html">Example transactional email</a></li>
+  <li><a href="example_insights-email.html">Example insights email</a></li>
+</ul>
+</body>
+
+</html>
